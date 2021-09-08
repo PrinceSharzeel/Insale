8868af8f4ef434458808b3381c6fc11e757c2ace
dPSudocoder <princesharzeel.10@gmail.com>
2021-09-08 23:22:12 +0530

    Update settings.py

--- insale/settings.py
+++ insale/settings.py
@@ -116,9 +116,9 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
