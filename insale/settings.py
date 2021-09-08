b2f55a1fc1a29e532acd749908583c638fd4c3ac
dPSudocoder <princesharzeel.10@gmail.com>
2021-09-08 23:11:06 +0530

    Update settings.py

--- insale/settings.py
+++ insale/settings.py
@@ -122,6 +122,6 @@ STATICFILES_DIRS = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
